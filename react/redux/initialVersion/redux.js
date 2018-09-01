65fd98fa40152ffba4cb9c8998f04d3ca9cd152d
EvalGitHub <957010831@qq.com>
2018-09-02 01:05:28 +0800

    add file

---
+++ react/redux/initialVersion/redux.js
@@ -0,0 +1,26 @@
+function createStore (state, stateChanger) {
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    stateChanger(state, action)
+    listeners.forEach((listener) => listener())
+  }
+  return { getState, dispatch, subscribe }
+}
+
+function stateChanger (state, action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      state.title.text = action.text
+      break
+    case 'UPDATE_TITLE_COLOR':
+      state.title.color = action.color
+      break
+    default:
+      break
+  }
+}
+
+
+
